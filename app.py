b8a6bccf578b29cf3d66ec67dd86416cdc20c406
Javeria78945577899 <javeria.15sep@gmail.com>
2024-12-30 18:43:26 -0600

    Update app.py

--- app.py
+++ app.py
@@ -189,7 +189,7 @@ if st.session_state.current_page == "Home":
                     )
                 # Add navigation button
                 cleaned_name = clean_category_name(category)
-                col.text_area(f"Go to {cleaned_name} Category through navigation bar")
+                col.button(f"Go to {cleaned_name} Category through navigation bar")
                     
                 
                 
